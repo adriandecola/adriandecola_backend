780f47ab88ef3f025844fe57cfb95ceb0f9de81d
Adrian deCola <adriandecola@gmail.com>
2023-12-19 12:29:44 -0500

    Update server so chat route saves message history

--- server.js
+++ server.js
@@ -21,9 +21,15 @@ const openai = new OpenAI(process.env.OPENAI_API_KEY);
 app.post('/chat', async (req, res) => {
   try {
     const userMessage = req.body.message;
+    // Get history from the request or initialize it
+    const messageHistory = req.body.messageHistory || [];
+
+    // Add the user's message to the history
+    messageHistory.push({ role: 'user', content: userMessage });
+
     const stream = await openai.chat.completions.create({
       model: 'gpt-3.5-turbo',
-      messages: [{ role: 'user', content: userMessage }],
+      messages: messageHistory,
       stream: true,
     });
 
@@ -31,13 +37,34 @@ app.post('/chat', async (req, res) => {
     res.setHeader('Cache-Control', 'no-cache');
     res.setHeader('Connection', 'keep-alive');
 
+    // Aggregates the assistant's response
+    let assistantResponse = '';
+
     for await (const chunk of stream) {
       if (chunk.choices[0]?.delta?.content) {
-        res.write(`data: ${chunk.choices[0].delta.content}\n\n`);
+        const part = chunk.choices[0].delta.content;
+        assistantResponse += part;
+        res.write(`data: ${part}\n\n`);
       }
     }
 
+    // Add the complete assistant's response to the history
+    messageHistory.push({
+      role: 'assistant',
+      content: assistantResponse.trim(),
+    });
+
+    // Ensure the history doesn't exceed 20 messages after adding the assistant's response
+    while (messageHistory.length > 20) {
+      messageHistory.shift();
+    }
+
+    // Send the full message history after the stream ends
+    res.write(
+      `data: ${JSON.stringify({ completeHistory: messageHistory })}\n\n`
+    );
     res.end();
+    console.log(messageHistory);
   } catch (error) {
     console.error(error);
     res.status(500).send(error.message);
